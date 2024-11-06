cda087e3024a961b37687ef861f1b5a51226d225
James D. Bartlett III <37491308+JamesDBartlett3@users.noreply.github.com>
2024-11-06 14:20:56 -0600

    Update ical2csv.py
    added Organizer and Attendee fields, disabled sorting

--- ical2csv.py
+++ ical2csv.py
@@ -8,12 +8,14 @@ import csv
 filename = sys.argv[1]
 # TODO: use regex to get file extension (chars after last period), in case it's not exactly 3 chars.
 file_extension = str(sys.argv[1])[-3:]
-headers = ('Summary', 'UID', 'Description', 'Location', 'Start Time', 'End Time', 'URL')
+headers = ('UID', 'Summary', 'Organizer', 'Attendee', 'Description', 'Location', 'Start Time', 'End Time', 'URL')
 
 class CalendarEvent:
     """Calendar event class"""
-    summary = ''
     uid = ''
+    summary = ''
+    organizer = ''
+    attendee = ''
     description = ''
     location = ''
     start = ''
@@ -35,19 +37,21 @@ def open_cal():
 
             for component in gcal.walk():
                 event = CalendarEvent("event")
-                if component.get('TRANSP') == 'TRANSPARENT': continue #skip event that have not been accepted
-                if component.get('SUMMARY') == None: continue #skip blank items
-                event.summary = component.get('SUMMARY')
                 event.uid = component.get('UID')
-                if component.get('DESCRIPTION') == None: continue #skip blank items
-                event.description = component.get('DESCRIPTION')
+                # if component.get('TRANSP') == 'TRANSPARENT': continue #skip event that have not been accepted
+                if component.get('SUMMARY') == None: continue # skip events without a summary
+                event.summary = component.get('SUMMARY')
+                if component.get('ORGANIZER') != None:
+                    event.organizer = component.get('ORGANIZER').params['CN'] + " <" + component.get('ORGANIZER') + ">"
+                if component.get('ATTENDEE') != None:
+                    event.attendee = component.get('ATTENDEE')
+                if component.get('DESCRIPTION') != None:
+                    event.description = component.get('DESCRIPTION')
                 event.location = component.get('LOCATION')
                 if hasattr(component.get('dtstart'), 'dt'):
                     event.start = component.get('dtstart').dt
                 if hasattr(component.get('dtend'), 'dt'):
                     event.end = component.get('dtend').dt
-
-
                 event.url = component.get('URL')
                 events.append(event)
             f.close()
@@ -68,7 +72,17 @@ def csv_write(icsfile):
             wr = csv.writer(myfile, quoting=csv.QUOTE_ALL)
             wr.writerow(headers)
             for event in sortedevents:
-                values = (event.summary.encode('utf8').decode(), event.uid, event.description.encode('utf8').decode(), event.location, event.start, event.end, event.url)
+                values = (
+                    event.uid, 
+                    event.summary.encode('utf8').decode(), 
+                    event.organizer, 
+                    event.attendee, 
+                    event.description.encode('utf8').decode(), 
+                    event.location, 
+                    event.start, 
+                    event.end, 
+                    event.url
+                )
                 wr.writerow(values)
             print("Wrote to ", csvfile, "\n")
     except IOError:
@@ -87,6 +101,7 @@ def debug_event(class_name):
     print(class_name.url, "\n")
 
 open_cal()
-sortedevents=sorted(events, key=lambda obj: obj.start) # Needed to sort events. They are not fully chronological in a Google Calendard export ...
+# sortedevents=sorted(events, key=lambda obj: obj.start) # Needed to sort events. They are not fully chronological in a Google Calendard export ...
+sortedevents = events
 csv_write(filename)
 #debug_event(event)
